f1372af394a5f47d70261b02e6782a676df39766
dark-devil-14 <rajuram9314@gmai.com>
2026-01-11 21:59:50 +0530

    Introduction file

---
+++ FirstPyProject.py
@@ -0,0 +1,5 @@
+import sys
+user_name = input('Enter You Name: ')
+print('\nMheta Familiy School of Data Science And Artificiall Intelligence')
+print('Hello @{}!!'.format(user_name))
+print(sys.version)
