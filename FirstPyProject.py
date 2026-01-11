a98f05d6ae9102d6afb5af954a163d3553a3602c
Rajuram D <rajuram9314@gmail.com>
2026-01-11 17:13:25 +0000

    done

--- FirstPyProject.py
+++ FirstPyProject.py
@@ -3,3 +3,5 @@ user_name = input('Enter You Name: ')
 print('\nMheta Familiy School of Data Science And Artificiall Intelligence')
 print('Hello @{}!!'.format(user_name))
 print(sys.version)
+
+print ('jello')
