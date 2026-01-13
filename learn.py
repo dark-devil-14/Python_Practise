84ffefc4e459d8dfee3fb6fe13b496d20094fd33
Rajuram D <rajuram9314@gmail.com>
2026-01-13 17:48:20 +0000

    practise_day2

--- learn.py
+++ learn.py
@@ -1,15 +1,29 @@
 #First Python Programming 
-# Credits : Prof Dr Neeraj Kumar Sharma   (IITG)
+#Credits : Prof Dr Neeraj Kumar Sharma   (IITG)
 
+#----------------------------------------------------------------------------------------------------------------
 #Variables ( Containers which store values )
 name = 'Jhon'
 age = 35
 total_cost = 32.12
 is_valid = True
-print(f"User: {name}   |   Age: {age}   |   Cost: {total_cost}   |   Valid: {is_valid}")
+print(f"\nUser: {name}   |   Age: {age}   |   Cost: {total_cost}   |   Valid: {is_valid}")
 print(type(name), type(age), type(total_cost), type(is_valid))
 
 x = "\nRajuram "
 y = "Bsc Student " 
 z = "2029"
 print(x+y+z)
+
+#----------------------------------------------------------------------------------------------------------------
+#Operating on Variables [parrentheses, exponential, division, multiplication, addition, subtraction] -- LEFT -> RIGHT -- 
+a = 2 * ( 3 + 4 ) ** 2
+print(a)
+
+#Logical operator
+a = True
+b = False
+
+c = a and b #AND Operator
+d = a or b #OR Operator
+print(c, d)
