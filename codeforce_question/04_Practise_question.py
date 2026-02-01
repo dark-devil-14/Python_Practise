ea44e8e6b2c68932c2769077a7c6d7eef79216cc
Rajuram D <rajuram9314@gmail.com>
2026-02-01 16:41:32 +0000

    Codeforce problem 4

---
+++ codeforce_question/04_Practise_question.py
@@ -0,0 +1,9 @@
+
+#  By darkdevil14, contest: Codeforces Beta Round 4 (Div. 2 Only), problem: (A) Watermelon, Accepted, #, Copy
+w = int(input())
+
+if(w%2==0 and w!=2 and w!= 0):
+    print("YES")
+else:
+    print("NO")
+
