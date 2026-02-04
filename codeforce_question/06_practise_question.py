0f6ef20faed673d2e7880c4934af57c94e3a9bff
Rajuram D <rajuram9314@gmail.com>
2026-02-04 18:49:30 +0000

    codeforce correction

--- codeforce_question/06_practise_question.py
+++ codeforce_question/06_practise_question.py
@@ -2,13 +2,24 @@
 num = list(map(int, input().split()))
 
 def is_prime(n):
+    if(n<2):
+        return False
+    
     for i in range(2, int(n**0.5)+1):
         if n%i == 0:
             return False
         
     return True
 
-if is_prime(num[0]) and is_prime(num[1]):
+new_num = num[0] + 1
+
+while True:
+    if is_prime(new_num):
+        break
+    new_num += 1
+
+
+if is_prime(num[0]) and is_prime(num[1]) and new_num == num[1] :
     print("YES")
 else:
     print("NO")
